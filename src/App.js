91fab8bd128fe5b222b5e7dc802ac64907e6540e
Nguyen-Van-Duy <nam1561999@gmail.com>
2022-06-28 23:31:00 +0700

    update box chat

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { dataFilmAction } from "./store/dataFilmSlice";
 import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import Loading from "./components/Loading";
+import ChatBox from "./components/ChatBox/ChatBox";
 // import MovieDetails from "./pages/MovieDetails/MovieDetails";
 // import ViewMore from "./pages/ViewMore/ViewMore";
 // import ViewGenre from "./pages/ViewGenre/ViewMore";
@@ -78,6 +79,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <ChatBox />
       <main className="main">
         {!isLoading && <Routes>
           <Route path="/" element={<Home />} />
---
+++ src/components/ChatBox/ChatBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./ChatBox.css"
+
+const ChatBox = () => {
+    return (
+        <div className="chat-box__containner">
+            
+        </div>
+    );
+};
+
+export default ChatBox;
