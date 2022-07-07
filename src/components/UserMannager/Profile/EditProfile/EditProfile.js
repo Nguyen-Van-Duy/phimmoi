625304944f9fdbf8f31bd5fdb5adcab4e602835c
Nguyen-Van-Duy <nam1561999@gmail.com>
2022-07-08 00:30:48 +0700

    update

--- src/components/UserMannager/Profile/EditProfile/EditProfile.js
+++ src/components/UserMannager/Profile/EditProfile/EditProfile.js
@@ -12,6 +12,7 @@ function EditProfile({handleShowEditProfile}) {
 
     const dataUser = useSelector((state) => state.loginSlice.dataUser);
     const [selectedFile, setSelectedFile] = useState();
+    const [image, setImage] = useState();
     const dispatch = useDispatch()
 
     const radioOptions = [
@@ -33,8 +34,14 @@ function EditProfile({handleShowEditProfile}) {
       console.log({...initialValues, test: "hi"});
 
     const handleChangeImage = (event) => {
-        // console.log(event.target.files[0], event.target);
+        console.log(event);
         setSelectedFile(event.target.files[0]);
+        console.log(event.target.files[0]);
+      };
+
+      const handleChangeImage2 = (event) => {
+        // console.log(event.target.files[0], event.target);
+        setImage(event.target.files[0]);
       };
    
     const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
@@ -50,8 +57,10 @@ function EditProfile({handleShowEditProfile}) {
         const dataRequest = {...values, user_id: dataUser._id}
         const formData = new FormData();
         formData.append('image_movie', selectedFile);
+        formData.append('image_movie2', image);
         formData.append('data', JSON.stringify(dataRequest));
         //  formData.append('password', "duy123");
+        console.log("formData", formData);
 
         const result = await axios.post(apiConfig.urlConnect + 'profile/upload', formData)
         dispatch(setUserId(result.data))
@@ -90,7 +99,7 @@ function EditProfile({handleShowEditProfile}) {
             </div>
             <div className='group'>
             <label htmlFor="image_movie" className="label">Avatar</label>
-                <input className='input' type="file" name="image_movie2" onChange={handleChangeImage} />
+                <input className='input' type="file" name="image_movie2" onChange={handleChangeImage2} />
             </div>
             <FormikControl
                 control='date'
