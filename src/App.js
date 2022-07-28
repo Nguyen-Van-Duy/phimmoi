e8f669648bcadbaae15f6f655135f31134324f9c
Nguyen-Van-Duy <nam1561999@gmail.com>
2022-07-29 00:49:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import 'antd/dist/antd.css'; 
 import "./App.css";
+import 'antd/dist/antd.css'; 
 import React,{ useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
